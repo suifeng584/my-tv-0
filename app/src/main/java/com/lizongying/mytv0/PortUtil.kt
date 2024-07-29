ef1f91d17bfa69d78082f7698f43120cb5843c3f
vrichv <1572349+vrichv@users.noreply.github.com>
2024-07-29 15:06:14 +0800

    Fixed the http server port to 10086

--- app/src/main/java/com/lizongying/mytv0/PortUtil.kt
+++ app/src/main/java/com/lizongying/mytv0/PortUtil.kt
@@ -8,15 +8,20 @@ import java.net.ServerSocket
 object PortUtil {
 
     fun findFreePort(): Int {
-        var port = -1
-        try {
-            ServerSocket(0).use { socket ->
-                port = socket.localPort
+        return try {
+            ServerSocket(10086).use { socket ->
+                socket.localPort
             }
         } catch (e: IOException) {
-            e.printStackTrace()
+            try {
+                ServerSocket(0).use { socket ->
+                    socket.localPort
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                -1 // Return -1 to indicate an error
+            }
         }
-        return port
     }
 
     fun lan(): String? {
